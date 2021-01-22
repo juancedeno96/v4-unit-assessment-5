ed08e7c87be695ad0ee93cb650fd56dab73c45af
juancedeno96 <cedenojj7@gmail.com>
2021-01-21 22:09:47 -0700

    completed user functions

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,25 +1,54 @@
-const bcrypt = require('bcryptjs');
-module.exports={
-register: async (req, res) => {
-    const {username, password} = req.body;
-    const profile_pic = `https://robohash.org/${username}.png`;
-    const db = req.app.get('db');
-    const result = await db.find_user_by_username([username])
-    const existingUser = result[0];
+const bcrypt = require("bcryptjs");
 
-    if (existingUser) {
-        return res.status(403).send('Username not available')
+module.exports = {
+  register: async (req, res) => {
+    const { username, password } = req.body;
+    const profile_pic = `https://robohash.org/${username}.png`;
+
+    const db = req.app.get("db");
+    const [result] = await db.user.find_user_by_username(username);
+
+    if (result) {
+      return res.status(403).send("Username not available");
     }
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
-    registeredUser = await db.create_user([username, hash, profile_pic]);
-    const user = registeredUser[0];
-    req.session.user = {
-        username: user.username,
-        profile_pic: user.profile_pic,
-        id: user.id
-    }
-    return res.status(201)
-},
+    [registeredUser] = await db.user.create_user([username, hash, profile_pic]);
 
-}
+    req.session.user = registeredUser;
+    return res.status(201).send(req.session.user);
+  },
+
+  login: async (req, res) => {
+    const { username, password } = req.body;
+    const db = req.app.get("db");
+
+    const [foundUser] = await db.user.find_user_by_username([username]);
+
+    if (!foundUser) {
+      return res
+        .status(401)
+        .send("Username not found, please create new account");
+    }
+    const verifyPassword = bcrypt.compareSync(password, foundUser.password);
+    if (!verifyPassword) {
+      return res.status(403).send("incorrect password");
+    }
+    delete user.password;
+
+    req.session.user = foundUser;
+    res.status(202).send(req.session.user);
+  },
+
+  logout: (req, res) => {
+    req.session.destroy();
+    return res.sendStatus(200);
+  },
+
+  getUser: (req, res) => {
+    if (!req.session.user) {
+      return res.sendStatus(401);
+    }
+    return res.status(200).send(req.session.user);
+  },
+};
